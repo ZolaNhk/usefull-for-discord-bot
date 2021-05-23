8dc5bb43ff6863769d92ac88fcf67f54d0065986
Zola nhk <61189739+ZolaNhk@users.noreply.github.com>
2021-05-23 14:16:30 +0200

    Update send.webhook.js

--- send.webhook.js
+++ send.webhook.js
@@ -2,20 +2,16 @@ const Discord = require('discord.js');
 const client = new Discord.Client(); 
 
 //-------"844222091501895760",
-const webhookClient = new Discord.WebhookClient("844222091501895760", "hkVtIkrgyaM7jOsfU1j5Q1d_CogGEl8JTAQmXiw5nwWZrOejASHs3KSEAgQY8SvXfFyq");
+const webhookClient = new Discord.WebhookClient("ID of webhook", "TOKEN OF WEBHOOK HERE, just the token not link");
 
 client.on("message", message => {
-let channel = message.channel;
-
-if(!message.content.startsWith(`>invite`)) return;
-channel.createInvite({unique: true})
-.then(invite => {
-	webhookClient.send("https://discord.gg/"+invite.code, {
+if(message.content === "test") {
+	webhookClient.send("your message", {
 	username: 'Onee San',
 	avatarURL: 'https://imgur.com/a/Ax02Axz',
 	
      })
-   })
+    }
 });
 
 client.login("token")
